d240eb4f9dd85fee86c90f8b80959d7b681ab543
1amcord <me@iamcord.de>
2019-02-25 09:55:33 +0100

    Initial

--- VirtualPlunger/Resource.h
+++ VirtualPlunger/Resource.h
@@ -1,30 +1,45 @@
 //{{NO_DEPENDENCIES}}
-// Von Microsoft Visual C++ generierte Includedatei.
-// Verwendet von VirtualPlunger.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_VIRTUALPLUNGER_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_VIRTUALPLUNGER			107
-#define IDI_SMALL				108
-#define IDC_VIRTUALPLUNGER			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Nächste Standardwerte für neue Objekte
+// Microsoft Developer Studio generated include file.
+// Used by Joystick.rc
 //
+#define IDI_MAIN                        102
+#define IDD_JOYST_IMM                   103
+#define IDR_ACCELERATOR1                103
+#define IDC_CLOSE                       1001
+#define IDC_X_AXIS                      1010
+#define IDC_Y_AXIS                      1011
+#define IDC_Z_AXIS                      1012
+#define IDC_X_AXIS_TEXT                 1013
+#define IDC_Y_AXIS_TEXT                 1014
+#define IDC_Z_AXIS_TEXT                 1015
+#define IDC_X_ROT_TEXT                  1016
+#define IDC_Y_ROT_TEXT                  1017
+#define IDC_Z_ROT_TEXT                  1018
+#define IDC_SLIDER0_TEXT                1019
+#define IDC_X_ROT                       1020
+#define IDC_Y_ROT                       1021
+#define IDC_Z_ROT                       1022
+#define IDC_SLIDER1_TEXT                1023
+#define IDC_POV0_TEXT                   1024
+#define IDC_POV1_TEXT                   1025
+#define IDC_POV2_TEXT                   1026
+#define IDC_POV3_TEXT                   1027
+#define IDC_SLIDER0                     1030
+#define IDC_SLIDER1                     1031
+#define IDC_POV                         1040
+#define IDC_POV0                        1040
+#define IDC_BUTTONS                     1041
+#define IDC_POV1                        1042
+#define IDC_POV2                        1043
+#define IDC_POV3                        1044
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1025
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
